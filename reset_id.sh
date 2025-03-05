e390a092c9621778ea19c6824b40abac4191e875
Killmonger27 <landryouarma45@gmail.com>
2025-03-05 10:26:40 +0000

    update reset_id.sh

--- reset_id.sh
+++ reset_id.sh
@@ -1,2 +1,2 @@
-ssh-keygen -f '~/.ssh/known_hosts' -R '192.168.56.11'
-ssh-keygen -f '~/.ssh/known_hosts' -R '192.168.56.12'
+ssh-keygen -f '/home/kill_m0nger27/.ssh/known_hosts' -R '192.168.56.11'
+ssh-keygen -f '/home/kill_m0nger27/.ssh/known_hosts' -R '192.168.56.12'
